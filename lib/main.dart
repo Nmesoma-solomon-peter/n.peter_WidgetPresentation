0d6306bcb402bb2ce069df527631d9409043e179
nmesoma <nmesomasolo2809@gmail.com>
2025-06-04 14:54:06 +0100

    (fix): add modifications on effective commenting to enable other developers understand the code

--- lib/main.dart
+++ lib/main.dart
@@ -13,24 +13,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'My Stack + Container Home page'),
     );
   }
 }
@@ -41,12 +26,6 @@ class MyHomePage extends StatefulWidget {
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
   // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -54,52 +33,36 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      // Appbar : is the the widget that we see on the topmost part of the scene
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
+        // the stack widget allow us to position a container on top of the other.  just like a stack of plates, here we will be placing a container on top of a  CircleAvatar
           child: Stack(
             children: [
-              CircleAvatar(radius: 50, backgroundImage: NetworkImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZvs3oYOxxCD8rgBXfoBEe2y7tUmHVjLty7Q&s")),
+              // The circle Avatar helps us create a rounded profile like image structure
+              CircleAvatar(radius: 50, backgroundImage: NetworkImage("https://africaleadership.net/wp-content/uploads/2016/07/Frederick.jpg")),
+              // the positioned widget helps us define where we want our container to be here we want it to be at the top right position
               Positioned(
                 top: 0,
                 right: 0,
                 child: Container(
+                  // I have used the padding attribute to add a padding of 5px to all sides of the container
                   padding: EdgeInsets.all(5),
+                  // Adding a green background color to the container using the decoration attribute and the BoxDecoration WIDGET
                   decoration: BoxDecoration(
                     color: Colors.green,
                     shape: BoxShape.circle,
                   ),
+                  // our container text with a with text color for visibility.
                   child: Text("5", style: TextStyle(color: Colors.white)),
                 ),
               ),
